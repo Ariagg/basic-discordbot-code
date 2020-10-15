56f50200976f49e29bb2189a3946786e151d83dd
Knwuh <58889981+Ariagg@users.noreply.github.com>
2020-10-15 22:03:38 +0700

    Update 2020

--- basicline.js.js
+++ basicline.js.js
@@ -8,7 +8,7 @@ const PREFIX = '^^'; //change whatever you like
 
 bot.on('ready', () => {
     console.log('bot is online!');
-    bot.user.setActivity('senja日没', { type: 'LISTENING'}).catch(console.error); //change the activity or status whatever you like
+    bot.user.setActivity('Change-Me', { type: 'LISTENING'}).catch(console.error); //change the activity or status whatever you like
 });
 
 bot.on('message', message=>{
@@ -18,14 +18,18 @@ bot.on('message', message=>{
         message.react("🔥") //you can change whatever emote you like
     }
     
+    if(!message.content.startsWith(PREFIX) || message.author.bot)return;    
     let args = message.content.substring(PREFIX.length).trim().split(" ");
     
     // command bot will be downhere
 
     switch(args[0]){
         case 'suggest':
-            message.reply('Suggestion recorded !, suggestion has been sent to a <@&736114093441810492> :)');
+            message.reply('Suggestion recorded !, suggestion has been sent to a @moderator')
         break;
+        case 'hi':
+            message.reply('hello there :D')
+            break;            
     }
 })
 
